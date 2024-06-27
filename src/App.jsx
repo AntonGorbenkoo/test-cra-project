6bfb5bf5cb5da13674d1ac44522ad4c84adec9cf
AntonGorbenkoo <antongorbenko@MacBook-Pro-Anton>
2024-06-27 07:41:21 +0200

    add camera

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import CameraComponent from './components/CameraComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <CameraComponent />
+    </div>
+  );
+}
+
+export default App;
